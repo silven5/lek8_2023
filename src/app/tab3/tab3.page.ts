7a210079ebee5aea0d03f4b394083526ea7291f5
silven <elvenff@gmail.com>
2023-04-07 13:09:58 +0300

    Commit

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,14 +1,32 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
-
+import { HttpClientModule } from '@angular/common/http';
+import { Post } from './class/post';
+import { PostService } from './srvice/post.service';
+import { CommonModule } from '@angular/common';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss'],
   standalone: true,
-  imports: [IonicModule, ExploreContainerComponent],
+  imports: [IonicModule, ExploreContainerComponent, HttpClientModule, CommonModule],
+  providers: [PostService]
 })
 export class Tab3Page {
-  constructor() {}
+  posts = new Array<Post>();
+  errorMessage: any;
+  private results!: Observable<Post[]>;
+  // створюємо сервіс
+  constructor(private service: PostService) { }
+  ngOnInit() {
+    this.posts = [];
+    this.service.getPosts().subscribe((response: Post[]) => {
+      let data: any = response;
+      this.posts = data.record;
+      console.log(this.posts);
+    });
+    console.log(this.posts);
+  }
 }
