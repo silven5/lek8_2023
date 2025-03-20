bbf21dd535207ff53467587233219957d140c90f
silven5 <elvenff@gmail.com>
2025-03-20 15:45:25 +0200

    commit 20/03/2025

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -8,8 +8,13 @@ import { Observable, pipe } from 'rxjs';
 import { interval, of, range } from 'rxjs';
 import { filter, map, tap } from 'rxjs/operators';
 //Приклад5
-import { fromEvent, from, } from 'rxjs';
-import { debounceTime, distinctUntilChanged, mergeMap, catchError } from 'rxjs/operators';
+import { fromEvent, from } from 'rxjs';
+import {
+  debounceTime,
+  distinctUntilChanged,
+  mergeMap,
+  catchError,
+} from 'rxjs/operators';
 import { ViewChild, ElementRef, OnInit } from '@angular/core';
 import { LoadingController } from '@ionic/angular';
 @Component({
@@ -27,7 +32,7 @@ export class Tab1Page {
   counter = 0;
   counter1 = 0;
 
-  constructor() { }
+  constructor() {}
   //Приклад1
   ras() {
     //почали стежити
@@ -38,24 +43,22 @@ export class Tab1Page {
     // просто логуємо кожне значення
     this.subscription = timer.subscribe({ next: console.log });
     this.counter++;
-    console.log("Запустили потік номер " + this.counter);
+    console.log('Запустили потік номер ' + this.counter);
   }
   stop1() {
     this.subscription.unsubscribe();
-    console.log("Стоп потік номер " + this.counter);
-
+    console.log('Стоп потік номер ' + this.counter);
   }
   //Приклад3
   ras2() {
     // просто логуємо кожне значення
     this.subscription1 = timer1.subscribe({ next: console.log });
     this.counter1++;
-    console.log("Запустили потік номер " + this.counter1);
+    console.log('Запустили потік номер ' + this.counter1);
   }
   stop2() {
     this.subscription1.unsubscribe();
-    console.log("Стоп потік номер " + this.counter1);
-
+    console.log('Стоп потік номер ' + this.counter1);
   }
   //Приклад 4
 
@@ -65,25 +68,21 @@ export class Tab1Page {
   data: number[] = [];
   // Приклад 4.1
   ras3_1() {
-    obs.subscribe(
-      val => {
-        this.data.push(val)
-        console.log(this.data)
-      }
-    )
-
+    obs.subscribe((val) => {
+      this.data.push(val);
+      console.log(this.data);
+    });
   }
   //Приклад 4_2
   ras4_2() {
     let data1: number[] = [];
-    array_o.subscribe(val => {
-      data1.push(val)
-      console.log(data1)
+    array_o.subscribe((val) => {
+      data1.push(val);
+      console.log(data1);
     });
   }
   //Приклад5
   @ViewChild('input', { static: true }) input!: ElementRef;
-
 
   gitHubUsers: string[] = [];
   recordRepsToList = (reps: string | any[]) => {
@@ -92,28 +91,25 @@ export class Tab1Page {
     for (let i = 0; i < reps.length; i++) {
       this.gitHubUsers.push(reps[i]);
     }
-
-  }
+  };
   ras5_1() {
-
     let gitUserService = new GitUserService();
     let user$ = fromEvent(this.input.nativeElement, 'keyup').pipe(
       //Встановлюємо затримку
 
       debounceTime(700),
       //отримуємо значення яке ввів користувач
-      map((event: any) => (event.target).value));
-    const subscribe = user$.subscribe(val => {
+      map((event: any) => event.target.value)
+    );
+    const subscribe = user$.subscribe((val) => {
       console.log(val);
       gitUserService.getUsersRepsFromAPI(val);
       this.recordRepsToList(gitUserService.name);
-
     });
-
   }
   // Приклад 6
   ob_prom() {
-    const s = new Observable(observer => {
+    const s = new Observable((observer) => {
       setTimeout(() => {
         observer.next('hi');
       }, 100);
@@ -125,7 +121,7 @@ export class Tab1Page {
       }, 300);
     });
 
-    s.subscribe(value => {
+    s.subscribe((value) => {
       console.log('observable:', value);
     });
 
@@ -142,30 +138,32 @@ export class Tab1Page {
       }, 300);
     });
 
-    p.then(value => {
+    p.then((value) => {
       console.log('promise: ', value);
     });
   }
 }
 //Приклад1
 //Створили потік який викидає декілька значень та закінчується
-const observable = new Observable(observer => {
+const observable = new Observable((observer) => {
   observer.next(1);
   observer.next(2);
   observer.next(25);
   observer.next(3 + 5);
-  observer.next("Hello Observable");
-  observer.next("HELLOOO!!!");
+  observer.next('Hello Observable');
+  observer.next('HELLOOO!!!');
+  observer.next(Math.sin(0.5));
+
   observer.complete();
-})
+});
 //створили спостирігача
 const observer = {
-  next: (value: any) => console.log(value), // 1, 2
+  next: (value: any) => console.log('Значення =' + value), // 1, 2
   error: (error: any) => console.error(error), //
-  complete: () => console.log("completed") // completed
+  complete: () => console.log('completed'), // completed
 };
 //Приклад2
-const timer = new Observable(observer => {
+const timer = new Observable((observer) => {
   // Оголошуємо x
   let x = -10;
   setInterval(() => {
@@ -174,51 +172,52 @@ const timer = new Observable(observer => {
     observer.next(x * x);
     // збільшуємо значення на одиницю
     x++;
-    console.log("Потік працює x=" + x);
+    console.log('Потік працює x=' + x);
   }, 1000);
 });
 //Приклад3
-const timer1 = new Observable(observer => {
+const timer1 = new Observable((observer) => {
   let x = -10;
   const intervalId = setInterval(() => {
     observer.next(x * x);
     x++;
-    console.log("Потік працює x=" + x);
+    console.log('Потік працює x=' + x);
   }, 1000);
   return () => {
     clearInterval(intervalId);
-  }
+  };
 });
 //Приклад4
-const timer2 = range(0, 10)
-  .pipe(
+const timer2 = range(0, 50).pipe(
+  filter((num) => num % 3 === 0),
 
-    filter((num) => num % 3 === 0),
-
-    map((num) => num * 2)
-
-  )
+  map((num) => num * 2)
+);
 // Приклад 4.1
 const obs = new Observable((observer) => {
-  observer.next(1)
-  observer.next(2)
-  observer.next(3)
-  observer.next(4)
-  observer.next(5)
-  observer.complete()
+  observer.next(1);
+  observer.next(2);
+  observer.next(3);
+  observer.next(4);
+  observer.next(5);
+  observer.complete();
 }).pipe(
   //tap Використовується для виконання побічних ефектів для сповіщень із observable
-  tap(data => console.log('tap ' + data)),
+  tap((data) => console.log('tap ' + data)),
   //filter Фільтрує дані за допомогою певної умови
   filter((data) => (data as number) > 2),
-  tap(data => console.log('filter ' + data)),        //tap
+  tap((data) => console.log('filter ' + data)), //tap
   //map - перетворює дані
-  map((val) => { return val as number * 2 }),
-  tap(data => console.log('final ' + data)),         //tap
-)
+  map((val) => {
+    return (val as number) * 2;
+  }),
+  tap((data) => console.log('final ' + data)) //tap
+);
 
 //Приклад 4.2
 const array_o = from([-2, -5, 4, 5, 6, -7, 22, -100]).pipe(
-  filter(num => num < 0),
-  map((val) => { return val as number * 2 })
-)
+  filter((num) => num < 0),
+  map((val) => {
+    return (val as number) * 2;
+  })
+);
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,100 +1,112 @@
-
 import { IonicModule } from '@ionic/angular';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 import { Component, OnInit } from '@angular/core';
 import { GitUserService } from './../git-service/git-user.service';
 import { interval, of, range, timer, fromEvent, from } from 'rxjs';
-import { filter, map, tap, mapTo, mergeMap, concatAll, mergeAll, switchAll, exhaust, debounceTime, distinctUntilChanged, switchMap, catchError } from 'rxjs/operators';
+import {
+  filter,
+  map,
+  tap,
+  mapTo,
+  mergeMap,
+  concatAll,
+  mergeAll,
+  switchAll,
+  exhaust,
+  debounceTime,
+  distinctUntilChanged,
+  switchMap,
+  catchError,
+} from 'rxjs/operators';
 import { ViewChild, ElementRef } from '@angular/core';
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   standalone: true,
-  imports: [IonicModule, ExploreContainerComponent]
+  imports: [IonicModule, ExploreContainerComponent],
 })
 export class Tab2Page {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  ngOnInit() {}
+  timer1$: any;
   //Приклад1
   ras() {
-    let timer1$ = timer(0, 1000).pipe(
-      map(() => makeRequest())
-    ).subscribe({
-      next: console.log
-    });
+    this.timer1$ = timer(0, 1000)
+      .pipe(map(() => makeRequest()))
+      .subscribe({
+        next: console.log,
+      });
   }
   //Приклад1_1
   ras1() {
-    timer(0, 1000).pipe(
-      mergeMap(() => makeRequest())
-    ).subscribe({
-      next: console.log
-    });
+    this.timer1$ = timer(0, 1000)
+      .pipe(mergeMap(() => makeRequest()))
+      .subscribe({
+        next: console.log,
+      });
   }
-
+  stop() {
+    this.timer1$.unsubscribe();
+  }
   //Приклад2
   ras2() {
-    console.log("ConcatAll");
+    console.log('ConcatAll');
     of(
       firstInnerObservable,
       secondInnerObservable,
       triInnerObservable,
       cInnerObservable,
       pInnerObservable
-    ).pipe(
-      concatAll()
-    ).subscribe({
-      next: console.log
-    });
+    )
+      .pipe(concatAll())
+      .subscribe({
+        next: console.log,
+      });
   }
   ras2_1() {
-    console.log("MergeAll");
+    console.log('MergeAll');
     of(
       firstInnerObservable,
       secondInnerObservable,
       triInnerObservable,
       cInnerObservable,
       pInnerObservable
-    ).pipe(
-      mergeAll()
-    ).subscribe({
-      next: console.log
-    });
+    )
+      .pipe(mergeAll())
+      .subscribe({
+        next: console.log,
+      });
   }
   ras2_2() {
-    console.log("switchAll");
+    console.log('switchAll');
     of(
       firstInnerObservable,
       secondInnerObservable,
       triInnerObservable,
       cInnerObservable,
       pInnerObservable
-    ).pipe(
-      switchAll()
-    ).subscribe({
-      next: console.log
-    });
+    )
+      .pipe(switchAll())
+      .subscribe({
+        next: console.log,
+      });
   }
   ras2_3() {
-    console.log("exhaust");
+    console.log('exhaust');
     of(
       firstInnerObservable,
       secondInnerObservable,
       triInnerObservable,
       cInnerObservable,
       pInnerObservable
-    ).pipe(
-      exhaust()
-    ).subscribe({
-      next: console.log
-    });
+    )
+      .pipe(exhaust())
+      .subscribe({
+        next: console.log,
+      });
   }
-
-
 }
 
 //Приклад1
@@ -103,9 +115,8 @@ const makeRequest = () => {
   return timer(1000).pipe(
     // mapTo('success')
     map(() => 'sucess')
-  )
-}
-
+  );
+};
 
 //Приклад2
 // потік, генерує 1 по після однієї секунди
@@ -128,8 +139,8 @@ const cInnerObservable = timer(1200).pipe(
   // mapTo(3)
   map(() => 4)
 );
-//  потік, генерує 5 по після однієї секунди
-const pInnerObservable = timer(1200).pipe(
+//  потік, генерує 10 по після однієї секунди
+const pInnerObservable = timer(150).pipe(
   // mapTo(3)
   map(() => 10)
 );
